c08a16d2edfe97579e8a1346b28391538a41a6c4
Jack Gilmore <jack.gilmore@nhs.scot>
2026-07-02 13:23:07 +0100

    feat: Popular datasets endpoint

---
+++ src/routes/payloads/popularDatasets.ts
@@ -0,0 +1,22 @@
+export const popularDatasetsPayload = {
+  date_range: "7d",
+  relative_date: new Date().toISOString().split('T')[0],
+  dimensions: ["event:page"],
+  metrics: ["visitors"],
+  filters: [["contains", "event:page", ["/datasets/"], { case_sensitive: false }]],
+  order_by: [
+    ["visitors", "desc"],
+    ["event:page", "asc"],
+  ],
+  pagination: { limit: 100, offset: 0 },
+  include: {
+    imports: true,
+    imports_meta: false,
+    time_labels: false,
+    partial_time_labels: false,
+    compare: null,
+    compare_match_day_of_week: true,
+    empty_metrics: false,
+    present_index: false,
+  },
+};
--- src/routes/stats.ts
+++ src/routes/stats.ts
@@ -1,38 +1,69 @@
-import { Hono } from "hono";
-import { applyRoutesToRouter, Handler, OpenApiRoute } from "../openapi";
+import { Hono } from 'hono';
+import { applyRoutesToRouter, Handler, OpenApiRoute } from '../openapi';
+import { popularDatasetsPayload } from './payloads/popularDatasets';
 
-const popularDatasetsHandler: Handler = (c) => {
-  return c.json({ "message": "hello world" });
+const popularDatasetsHandler: Handler = async (c) => {
+	console.log(popularDatasetsPayload);
+
+	const response = await fetch('https://plausible.io/api/stats/opendata.scot/query/', {
+		method: 'POST',
+		body: JSON.stringify(popularDatasetsPayload),
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	});
+
+	const body = (await response.json()) as any;
+
+	const filteredResults = body.results?.filter((row: any) => row.dimensions?.[0] !== '/datasets/');
+
+	const transformedResults = filteredResults
+		?.map((row: any) => ({
+			page: row.dimensions?.[0],
+			visitors: row.metrics?.[0],
+		}))
+		.slice(0, 5);
+
+	return c.json(transformedResults);
 };
 
 export const statsRoutes: OpenApiRoute[] = [
-  {
-    method: "get",
-    path: "/popular-datasets",
-    handler: popularDatasetsHandler,
-    openapi: {
-      summary: "Popular datasets",
-      description: "Returns a hello world message to demonstrate the stats router.",
-      tags: ["Statistics"],
-      responses: {
-        "200": {
-          description: "A friendly confirmation",
-          content: {
-            "application/json": {
-              schema: {
-                type: "object",
-                properties: {
-                  message: { type: "string" },
-                },
-                required: ["message"],
-              },
-              example: { message: "hello world" },
-            },
-          },
-        },
-      },
-    },
-  },
+	{
+		method: 'get',
+		path: '/popular-datasets',
+		handler: popularDatasetsHandler,
+		openapi: {
+			summary: 'Popular datasets',
+			description: 'Returns the most popular datasets pages in the last 7 days.',
+			tags: ['Statistics'],
+			responses: {
+				'200': {
+					description: 'A list of the most popular datasets pages in the last 7 days.',
+					content: {
+						'application/json': {
+							schema: {
+								type: 'array',
+								items: {
+									type: 'object',
+									properties: {
+										page: { type: 'string' },
+										visitors: { type: 'number' },
+									},
+									required: ['page', 'visitors'],
+								},
+							},
+							example: [
+								{
+									page: '/datasets/dataset-name/',
+									visitors: 10,
+								},
+							],
+						},
+					},
+				},
+			},
+		},
+	},
 ];
 
 const app = new Hono();
